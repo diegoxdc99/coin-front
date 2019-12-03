b75eb785006bb8828936b44ff94302f59f4adeea
Diego Alberto Meneses Tamayo <diego.meneses@sophos.col.com>
2019-12-03 08:56:40 -0500

    reactive validations and message errors

--- src/app/coins/components/exchange/exchange.component.ts
+++ src/app/coins/components/exchange/exchange.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CurrencyInput } from '../../../model/CurrencyInput';
 
 @Component({
   selector: 'app-exchange',
@@ -8,22 +9,55 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class ExchangeComponent implements OnInit {
   exchangeForm: FormGroup;
+  currencies: Array<CurrencyInput>;
+
+  toQuantity: number;
+
+  validationMessages = {
+    fromQuantity: [
+      { type: 'required', message: 'La cantidad a convertir es requerida.' },
+      { type: 'min', message: 'La cantidad a convertir debe ser mayor a 0' }
+    ],
+    from: [
+      { type: 'required', message: 'La criptomoneda/moneda es requerida'},
+    ],
+    to: [
+      { type: 'required', message: 'La criptomoneda/moneda es requerida'},
+    ]
+  };
 
   constructor(
     private formBuilder: FormBuilder
   ) {
     this.buildForm();
+    this.currencies = [
+      {
+        label: 'peso colombiano',
+        value: 'COP'
+      },
+      {
+        label: 'bitcoin',
+        value: 'BTC'
+      }
+    ];
+    this.toQuantity = 5000;
   }
 
   buildForm() {
     this.exchangeForm = this.formBuilder.group({
+      fromQuantity: [null, [Validators.required, Validators.min(1)]],
       from: [null, [Validators.required]],
-      to: [null, [Validators.required]],
-      amount: [null, [Validators.required, Validators.min(1)]]
+      to: [null, [Validators.required]]
     });
   }
 
   ngOnInit() {
+  }
+
+  submit() {
+    if (this.exchangeForm.valid) {
+      alert('valido');
+    }
   }
 
 }
---
+++ src/app/model/CurrencyInput.ts
@@ -0,0 +1,4 @@
+export interface CurrencyInput {
+  value: string;
+  label: string;
+}
